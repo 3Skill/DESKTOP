8719074f0d76f1ae7f2e814c17ec2834b199c2ff
victormerch <1Victormerch@gmail.com>
2021-11-29 19:22:13 +0100

    Implementacion: funciones para saber informacion del kahoot para el cliente añadidas

--- DESKTOP/src/com/example/kadamm/ui/connexio/InterRMI.java
+++ DESKTOP/src/com/example/kadamm/ui/connexio/InterRMI.java
@@ -1,10 +1,14 @@
 package com.example.kadamm.ui.connexio;
 
+import java.util.ArrayList;
 
 public interface InterRMI{
 	public void setNickName(String nickname);
 	public boolean getWaitingRoomStatus();
+	public boolean getWaitingRoom2Status();
 	public boolean isUserAvailable(String nickname);
+	public ArrayList<String> getConcurs(int i);
+	
 }
 
 //ConnRMILipe
